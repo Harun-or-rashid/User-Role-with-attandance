82edb715cfde8a2f584213193d381af820ffcad4
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-07-08 02:21:24 +0600

    relation added in user and role model

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $guarded=[];
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+
+}
 }
--- app/User.php
+++ app/User.php
@@ -10,6 +10,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function roles()
+    {
+        $this->hasMany(Role::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
