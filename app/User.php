a9a53f4c5f879b3558e0113af638dc0100ea8b3f
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-07-09 01:58:31 +0600

    middleware route added

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 
     public function roles()
     {
-        $this->hasMany(Role::class);
+        $this->hasMany(Role::class,'role_id','id');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,19 @@ Route::post('store','EmployeeController@store');
 Route::get('signin/','LoginController@showLogin')->name('signin');
 Route::post('login','LoginController@login')->name('login');
 Route::get('out','LoginController@logOut')->name('logout');
+Route::group(['middleware'=>['auth']],function (){
+    Route::prefix('employees')->name('employee.')->group(function (){
 
-Route::prefix('employees')->name('employee.')->group(function (){
-    Route::get('/','EmployeeController@index')->name('index');
-    Route::get('/show/{id}','EmployeeController@show')->name('show');
-    Route::get('/create','EmployeeController@create')->name('create');
-    Route::post('/store','EmployeeController@store')->name('store');
-    Route::get('/edit/{id}','EmployeeController@edit')->name('edit');
-    Route::post('/update/{id}','EmployeeController@update')->name('update');
-    Route::get('/delete/{id}','EmployeeController@destroy')->name('delete');
+        Route::get('/','EmployeeController@index')->name('index');
+        Route::get('/show/{id}','EmployeeController@show')->name('show');
+        Route::get('/create','EmployeeController@create')->name('create');
+        Route::post('/store','EmployeeController@store')->name('store');
+        Route::get('/edit/{id}','EmployeeController@edit')->name('edit');
+        Route::post('/update/{id}','EmployeeController@update')->name('update');
+        Route::get('/delete/{id}','EmployeeController@destroy')->name('delete');
 
+    });
 });
+
+
 
